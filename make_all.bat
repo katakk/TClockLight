13fbec5031a01d331e147162c60cfde3f89fe2ff
桜あかりちゃんだよ♥ <kata@katakuri.sakura.ne.jp>
2017-01-12 07:50:21 +0900

    hoge

--- make_all.bat
+++ make_all.bat
@@ -1,13 +1,6 @@
 @echo off
 
-echo ===============================
-echo Building normal version
-echo ===============================
 cd source
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-nmake /nologo clean
-nmake /nologo
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-nmake /nologo
+nmake /nologo %*
 if errorlevel 1 goto error_exit
 cd ..
