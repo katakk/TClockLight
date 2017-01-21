fb63804edad605102796b7fa8f1c1870695fe3bc
katakk <kata@katakuri.sakura.ne.jp>
2017-01-21 18:27:19 +0900

    delete unused flags

--- source/common/command.h
+++ source/common/command.h
@@ -1,32 +1,3 @@
-// TClock commands
-#define IDC_SHOWPROP                    100
-#define IDC_SHOWHELP                    101
-#define IDC_EXIT                        102
-#define IDC_TIMER                       103
-#define IDC_SYNCTIME                    104
-#define IDC_SHOWCALENDER                105
-#define IDC_2CHHELP                     106
-#define IDC_COPYCLIP                    107
-#define IDC_ABOUT                       108
 
-#define IDC_REFRESHCLOCK                109
-#define IDC_STOPSOUND                   110
-#define IDC_PAUSERESUME                 111
-#define IDC_PREVTRACK                   112
-#define IDC_NEXTTRACK                   113
-
-#define IDC_TCLOCKMENU                  121
-#define IDC_SCREENSAVER                 122
-#define IDC_KYU                         123
-#define IDC_DELRECDOCS                  124
-#define IDC_SHOWSTARTMENU               125
-#define IDC_TASKSW                      126
-#define IDC_SHOWDESK                    127
-#define IDC_LOCKPC                      128
-#define IDC_CHANGECONF                  129
-#define IDC_VOLMUTE                     130
-#define IDC_MONOFF                      131
-#define IDC_NETINIT                     132
-#define IDC_DELUS                       133
 
 
